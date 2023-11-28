7f9b066dd64bd74403ef78c18ae45ea98a32cfb0
Pethum Jeewantha <me@pethumjeewantha.com>
2023-11-28 08:23:06 +0530

    style active links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' }
@@ -15,7 +21,11 @@ const NavBar = () => {
                 {links.map((link) =>
                     <Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classNames({
+                            'text-zinc-900': currentPath === link.href,
+                            'text-zinc-500': currentPath !== link.href,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
                         href={link.href}>
                         {link.label}
                     </Link>)}
