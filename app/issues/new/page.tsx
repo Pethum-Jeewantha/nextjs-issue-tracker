21bf3e22b04bfe4c7371c56b50fefa9af156b698
Pethum Jeewantha <me@pethumjeewantha.com>
2023-11-28 23:42:03 +0530

    submit issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,46 @@
 'use client'
 
-import React from 'react'
-import { Button, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form'
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+    title: string
+    description: string
+}
 
 const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root>
-        <TextField.Input placeholder='Title'/>
-      </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Issue</Button>
-    </div>
-  )
+    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const router = useRouter();
+    const [error, setError] = useState('');
+
+    return (
+        <div className='max-w-xl'>
+            {error && <Callout.Root color='red' className='mb-5'><Callout.Text>{error}</Callout.Text></Callout.Root>}
+            <form className='space-y-3' onSubmit={handleSubmit(async (data) => {
+                try {
+                    await axios.post('/api/issues', data);
+                    router.push('/issues');
+                } catch (error) {
+                    setError('An unexpected error occurred')
+                }
+            })}>
+                <TextField.Root>
+                    <TextField.Input placeholder='Title' {...register('title')} />
+                </TextField.Root>
+                <Controller
+                    name='description'
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
+    )
 }
 
 export default NewIssuePage
