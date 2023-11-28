fb4329a8cb4133d3fdd13d2127d59dae5cfc890e
Pethum Jeewantha <me@pethumjeewantha.com>
2023-11-28 21:25:45 +0530

    setup redix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const IssuesPage = () => {
   return (
     <div>
-      
+      <Button>New Issue</Button>
     </div>
   )
 }
