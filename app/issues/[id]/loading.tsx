976da7e6631b72a77df2bc11a400d5ed799741a5
Pethum Jeewantha <me@pethumjeewantha.com>
2023-11-30 23:56:36 +0530

    additional loading

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Box, Card, Flex } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>
-      loading...
-    </div>
+    <Box className='max-w-xl'>
+      <Skeleton />
+      <Flex gap='3' my='2'>
+        <Skeleton width='5rem' />
+        <Skeleton width='8rem' />
+      </Flex>
+      <Card className='prose' mt='4'><Skeleton count={3}/></Card>
+    </Box>
   )
 }
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
-import delay from 'delay'
 import IssueActions from './IssueActions'
 import Link from '../components/Link'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000)
 
   return (
     <div>
