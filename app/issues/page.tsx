5c9bd93c539655a8ef92393f02fb3dedcca589b1
Pethum Jeewantha <me@pethumjeewantha.com>
2023-11-30 20:54:23 +0530

    add loading

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
+import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
+import delay from 'delay'
+import IssueActions from './IssueActions'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
+  await delay(2000)
 
   return (
     <div>
-      <div className='mb-5'>
-        <Button><Link href='/issues/new'>New Issue</Link></Button>
-      </div>
+      <IssueActions />
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
