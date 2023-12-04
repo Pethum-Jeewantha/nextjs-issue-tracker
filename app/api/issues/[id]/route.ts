c50ef1edf7b1dc654e5cdb51e4a9f4c0ada53726
Pethum Jeewantha <me@pethumjeewantha.com>
2023-12-04 20:28:05 +0530

    delete issue api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -23,3 +23,13 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
     return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } });
+    if (!issue)
+        return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+    prisma.issue.delete({ where: { id: issue.id } });
+
+    return NextResponse.json({id: issue.id});
+}
