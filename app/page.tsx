dd43081b93991616c585286dde7f4ed56cbf33eb
Pethum Jeewantha <me@pethumjeewantha.com>
2023-12-10 13:15:36 +0530

    create issuechart comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import prisma from "@/prisma/client";
@@ -8,6 +9,6 @@ export default async function Home() {
   const closed = await prisma.issue.count({where: {status: "DONE"}})
 
   return (
-    <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    <IssueChart open={open} inProgress={inProgress} closed={closed} />
   )
 }
