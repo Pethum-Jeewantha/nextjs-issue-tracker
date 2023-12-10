5ebf7bcefe8fe69ad91a4842c4c9b708e33bd28c
Pethum Jeewantha <me@pethumjeewantha.com>
2023-12-10 13:05:01 +0530

    create issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({where: {status: "OPEN"}})
+  const inProgress = await prisma.issue.count({where: {status: "IN_PROGRESS"}})
+  const closed = await prisma.issue.count({where: {status: "DONE"}})
+
   return (
-    <LatestIssues />
+    <IssueSummary open={open} inProgress={inProgress} closed={closed} />
   )
 }
